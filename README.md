4f862f7ce606de0966c533ca257edbc00c5c0bf3
james c rubino <james dot rubino at google mail>
2018-11-03 21:01:49 -0500

    stages build script

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # GobyteDocker
-Dockerfiles for Building and Running Gobyte Containers for local regression tests.  
+Dockerfiles for building and running [Gobyte](https://www.gobyte.network/) node and masternode containers for local regression tests; or for easier deployments.
 Currently build-flow is in stages to easily configure up-to-date images as needed.
 Can be done in one step, with Alpine lib removals during build process.
 
---
+++ stages/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+docker build -f gobyte-alpine-base.Dockerfile -t gobyte-alpine-base .
+docker build -f gobyte-alpine-compile.Dockerfile -t gobyte-alpine-compile .
+docker build -f gobyte-alpine-binaries.Dockerfile -t gobyte-alpine-binaries .
+cp ../conf/gobyte-node.conf .
+docker build -f gobyte-alpine-node.Dockerfile -t gobyte-node .
+rm gobyte-node.conf
+cp ../conf/gobyte-masternode.conf .
+docker build -f gobyte-alpine-mn.Dockerfile -t gobyte-masternode .
+rm gobyte-masternode.conf
